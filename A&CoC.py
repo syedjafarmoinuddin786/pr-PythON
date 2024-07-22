ffd6424fad30bae58229ef66cb75a86a0f6af6d5
SJM8786 <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-07-22 14:55:12 +0530

    A&CoC.py

---
+++ A&CoC.py
@@ -0,0 +1,9 @@
+# AREA & CIRCUMFERENCE OF CIRCLE
+d=float(input("diameter of the circle(in cm):"))
+r=float(d/2)
+print("Area of the Circle(in cm):")
+AoC=(3.14*(r*r))
+print(AoC)
+print("Circumference of the Circle(in cm):")
+CoC=(2*3.14)*r
+print(CoC)
