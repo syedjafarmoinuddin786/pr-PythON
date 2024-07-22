6bc476bafd45c2b84ab8abd6984848243b2dbf86
SJM8786 <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-07-22 14:43:43 +0530

    AoRe.py

---
+++ AoRe.py
@@ -0,0 +1,5 @@
+l=float(input("ENTER THE LENGTH OF THE RECTANGLE(in cm):"))
+b=float(input("ENTER THE BREADTH OF TH RECTANGLE(in cm):"))
+print("AREA OF THE RECTANGLE(in cm):")
+Area=l*b
+print(Area) 
