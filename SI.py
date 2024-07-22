e6cec7236f8d87997583e21c3f8e1a2bd6f84761
SJM8786 <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-07-22 14:19:59 +0530

    SI.py

---
+++ SI.py
@@ -0,0 +1,6 @@
+p=int(input("ENTER THE PRINCIPAL AMOUNT:"))
+t=float(input("ENTER THE TIME PERIOD:"))
+r=float(input("ENTER THE  RATE OF INTEREST:"))
+SI=(p*t*r)/100
+print("SIMPLE INTEREST:Rs.")
+print(SI)
