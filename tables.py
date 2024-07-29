1aad473ae241b2fe5bfb2abdb2e111effc598baa
SJM8786 <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-07-29 15:51:43 +0530

    printtables.py

---
+++ tables.py
@@ -0,0 +1,14 @@
+#PRINT A MULTIPLICATION TABLE using for loop
+i=int(1)
+n=int(input())
+for i in range(1,11):
+  print(n,"*",i,"=",n*i)
+#PRINT A MULTIPLICATION TABLE using while loop
+i=int(1)
+n=int(input())
+while(i<=10):
+  print(n,"*",i,"=",n*i)
+  i=i+1
+ 
+
+
