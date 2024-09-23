dc11cf4972de1d154fbf8e0a15476ca89ff65820
S_Jafar_M <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-09-23 15:54:28 +0530

    printing reverse of the list using list fun.py

---
+++ listreverse.py
@@ -0,0 +1,8 @@
+#TO REVERSE THE LIST ELEMENTS using REVERSE FUN
+l = []
+n = int(input("ENTER THE RANGE OF THE LIST:"))
+for i in range(n):
+    lis = int(input())
+    l.append(lis)
+l.reverse()
+print(l)     
