dd682db611388d7e91e13e430f5a6870d5ab588a
S_Jafar_M <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-10-21 15:25:45 +0530

    arithmeticop.py
    USING FUNCTIONS

---
+++ Week_07/arithmeticop.py
@@ -0,0 +1,15 @@
+def su(a,b):
+	print("Sum of the Values ",(a+b))
+def sub(a,b):
+	print("Difference of the two numbers",(a-b))
+def mul(a,b):
+	print("Multiplication of the numbers",(a*b))
+def div(a,b):
+	print("Divison of the numbers",(a/b))					
+print("ENTER THE VALUES:")
+a = int(input())
+b = int(input())
+su(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
