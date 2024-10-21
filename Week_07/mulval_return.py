ed9e6077bcf704192deebca5c53534b2a97de283
S_Jafar_M <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-10-21 15:59:28 +0530

    Create mulval_return.py

---
+++ Week_07/mulval_return.py
@@ -0,0 +1,11 @@
+# wrtie a program to define a function which returns multiple values
+# let us take the example of the rectangle
+print("ENTER THE VALUES OF LENGTH AND BREADTH:") 
+l = int(input())
+b = int(input())
+def area_and_perimeter_rect(l,b):
+	area = l*b
+	perimeter = 2*(l+b)
+	return area,perimeter 
+print("AREA OF THE RECATNGLE",area_and_perimeter_rect(l,b))
+ 
